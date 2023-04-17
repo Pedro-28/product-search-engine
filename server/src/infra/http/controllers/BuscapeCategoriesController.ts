031673450bd4135cc700007567fc72ef2383d1c7
Pedro-28 <pedrohdp2017@gmail.com>
2023-04-17 20:20:24 -0300

    Adding some console.logs to handle errors

--- server/src/infra/http/controllers/BuscapeCategoriesController.ts
+++ server/src/infra/http/controllers/BuscapeCategoriesController.ts
@@ -15,6 +15,7 @@ export class BuscapeCategoriesController {
         products: products.map(ProductViewModel.toHTTP),
       };
     } catch (error) {
+      console.log(error);
       reply.code(500).send("Internal error");
     }
   }
